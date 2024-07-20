74d2f1ca268273ee367c671060c208e95d20a3ed
raul.saraujo <raul.araujo.2000@hotmail.com>
2024-07-20 00:35:31 -0300

    Feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+
+  typescript: {
+    typeCheck: true
+  },
+
+  modules: ["@nuxt/eslint"],
+
+  sourcemap: {
+    server: true,
+    client: true
+  }
+})
