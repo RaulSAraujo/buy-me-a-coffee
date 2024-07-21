229a20115d3d20f846fc47d59a7e8d968afd9c74
raul.saraujo <raul.araujo.2000@hotmail.com>
2024-07-21 12:41:50 -0300

    feat: support card component

---
+++ components/CardSupport.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-card
+    class="mb-3 border-sm"
+    title="💍🌟 Apoie meu Sonho de Casamento!"
+    elevation="0"
+    rounded="xl"
+  >
+    <v-card-text>
+      <p class="text-overline">Olá, amigos e comunidade!</p>
+      <p class="text-caption">
+        Eu sou um desenvolvedor web apaixonado, sempre em busca de criar soluções
+        incríveis com tecnologias como Vue.js, Node.js e Flutter. Dedico meu tempo e
+        esforço para me tornar cada vez melhor na minha profissão, e isso me traz uma
+        enorme satisfação.
+      </p>
+      <p class="text-caption">
+        Agora, estou em um momento muito especial da minha vida: planejo me casar com a
+        pessoa que amo! 💕 No entanto, os custos de um casamento podem ser bastante altos,
+        e eu preciso da sua ajuda para tornar esse sonho realidade.
+      </p>
+      <p class="text-caption">
+        Toda doação, não importa o tamanho, fará uma diferença imensa. Com o seu apoio,
+        poderei não apenas realizar meu sonho de casamento, mas também continuar
+        investindo em minha carreira e aprimorando minhas habilidades como programador.
+      </p>
+      <p class="text-caption">
+        Acredite, cada cafezinho que você normalmente me oferece pode se transformar em um
+        passo mais próximo do altar! ☕👰🤵
+      </p>
+      <p class="text-caption">
+        Se você já se emocionou com algum projeto meu ou se acredita no poder do amor e na
+        importância de seguir os nossos sonhos, peço humildemente o seu apoio.
+      </p>
+      <p class="text-caption">
+        Vamos juntos transformar este momento em um marco inesquecível, cheio de amor e
+        novas oportunidades para continuar crescendo e contribuindo com a comunidade de
+        desenvolvimento!
+      </p>
+      <p class="text-caption">Muito obrigado pelo seu carinho e suporte! ❤️</p>
+      <p class="text-caption">Com gratidão,<br />Raul Silva Araujo</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style lang="css" scoped>
+p {
+  margin: 10px 0px;
+}
+</style>
