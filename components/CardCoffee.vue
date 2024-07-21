6e82dbdea26166587d56d6163b15a9844f9712b9
raul.saraujo <raul.araujo.2000@hotmail.com>
2024-07-21 12:40:10 -0300

    Refactor: card coffee

--- components/CardCoffee.vue
+++ components/CardCoffee.vue
@@ -20,24 +20,31 @@ const _srcset = computed(() => {
 </script>
 
 <template>
-  <v-card rounded="xl" elevation="1">
+  <v-card class="border-sm" rounded="xl" elevation="0" height="410">
     <v-img
       :lazy-src="img(src, { quality: 50 })"
       :src="img(src, { quality: 100 })"
       :srcset="_srcset.srcset"
+      height="200"
       cover
       class="align-end text-white"
       gradient="to top, rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0)"
     >
-      <v-card-title>{{ title }}</v-card-title>
+      <v-card-title class="text-overline">{{ title }}</v-card-title>
     </v-img>
 
-    <v-card-text>
+    <v-card-text class="text-caption">
       {{ description }}
     </v-card-text>
 
-    <v-card-actions>
-      <v-btn color="orange" :text="`DONATION ${price}`" block />
-    </v-card-actions>
+    <template #actions>
+      <v-btn
+        color="orange"
+        :text="`DONATION ${price}`"
+        block
+        position="absolute"
+        location="bottom center"
+      />
+    </template>
   </v-card>
 </template>
